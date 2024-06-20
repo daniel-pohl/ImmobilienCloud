e5a7c090e955e208763a84afdf8ce6edcb59d790
daniel-pohl <danielpohl@mail.de>
2024-06-20 11:25:51 +0200

    Company integratiosntests fixed

--- backend/src/test/java/de/neuefische/backend/company/CompanyControllerIntegrationTest.java
+++ backend/src/test/java/de/neuefische/backend/company/CompanyControllerIntegrationTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -56,24 +55,8 @@ class CompanyControllerIntegrationTest {
     @DirtiesContext
     @Test
     void getAllCompanies_whenOneCompanyInDB_thenReturnListOfOne() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/company")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("""
-                          [
-                            {
-                                  "name": "TestFirma3",
-                                  "country": "TestCountry3",
-                                  "city": "TestCity3",
-                                  "plz": "133335",
-                                  "street": "TestStreet",
-                                  "streetNumber": "3",
-                                  "phoneNumber": "123-3333-7890",
-                                  "email": "test3@example.com",
-                                  "website": "http://www.testfirma3.com",
-                                  "comment": "This is a test comment333."
-                            }
-                        ]
-                        """));
+        Company company = new Company(null, "TestFirma3", "TestCountry3", "TestCity3", "133335", "TestStreet", "3", "123-3333-7890", "test3@example.com", "http://www.testfirma3.com", "This is a test comment333.");
+        companyRepo.save(company);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/company"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -107,21 +90,21 @@ class CompanyControllerIntegrationTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/company/123"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json("""
-                        [
-                                                    {
-                                                          "id": "123",
-                                                          "name": "TestFirma3",
-                                                          "country": "TestCountry3",
-                                                          "city": "TestCity3",
-                                                          "plz": "133335",
-                                                          "street": "TestStreet",
-                                                          "streetNumber": "3",
-                                                          "phoneNumber": "123-3333-7890",
-                                                          "email": "test3@example.com",
-                                                          "website": "http://www.testfirma3.com",
-                                                          "comment": "This is a test comment333."
-                                                    }
-                                                ]
+                        
+                                        {
+                                              "id": "123",
+                                              "name": "TestFirma3",
+                                              "country": "TestCountry3",
+                                              "city": "TestCity3",
+                                              "plz": "133335",
+                                              "street": "TestStreet",
+                                              "streetNumber": "3",
+                                              "phoneNumber": "123-3333-7890",
+                                              "email": "test3@example.com",
+                                              "website": "http://www.testfirma3.com",
+                                              "comment": "This is a test comment333."
+                                        }
+                        
                         """));
     }
 }
