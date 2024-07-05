283cce64207e027a4678356f25dfeb14837f603d
daniel-pohl <danielpohl@mail.de>
2024-07-05 22:52:19 +0200

    Add login test addOn

--- backend/src/test/java/de/neuefische/backend/appuser/AppUserControllerIntegrationTest.java
+++ backend/src/test/java/de/neuefische/backend/appuser/AppUserControllerIntegrationTest.java
@@ -6,8 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -22,21 +23,23 @@ class AppUserControllerIntegrationTest {
     @Autowired
     ObjectMapper objectMapper;
 
+
     @Test
-    void getLoggedInUser_shouldReturnUserDetails_whenUserIsLoggedIn() throws Exception {
+    @WithAnonymousUser
+    void getLoggedInUser_shouldReturnUnauthorized_whenUserIsNotLoggedIn() throws Exception {
         mockMvc.perform(get("/api/users/me"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.username").value("loggedInUser"));
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
+    @WithMockUser
     void login_shouldAllowUserToLogin() throws Exception {
         mockMvc.perform(post("/api/users/login"))
                 .andExpect(status().isOk());
     }
 
     @Test
+    @WithMockUser
     void register_shouldCreateNewUser_whenValidInput() throws Exception {
         AppUserRegister newUser = new AppUserRegister("newUser", "password");
 
@@ -49,6 +52,7 @@ class AppUserControllerIntegrationTest {
     }
 
     @Test
+    @WithMockUser
     void logout_shouldInvalidateSession_whenUserLogsOut() throws Exception {
         mockMvc.perform(post("/api/users/logout"))
                 .andExpect(status().isNoContent());
